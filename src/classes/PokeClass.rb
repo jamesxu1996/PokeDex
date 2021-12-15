ce4cbfed8798dbc419633529e580b3965c44d4d1
James Xu <jamesxu_1996@hotmail.com>
2021-12-15 23:26:03 +1100

    reasks user for name if empty input user_name

--- src/classes/PokeClass.rb
+++ src/classes/PokeClass.rb
@@ -11,6 +11,7 @@ require "tty-prompt"
 
 
 
+
 class Pokedex
     attr_accessor :name
 
@@ -18,39 +19,72 @@ class Pokedex
         @name = name
     end
 
-    def getPokemonGeneralInfo(pokemonName) 
-        res = PokeApi.get(pokemon: pokemonName)
-        pokemonType = []
+    def get_pokemon_general_info(pokemon_name) 
+        res = PokeApi.get(pokemon: pokemon_name)
+        pokemon_type = []
 
         res.types.each do |item| 
-            pokemonType << item.type.name
+            pokemon_type << item.type.name.capitalize
+            # pokemonType.colorize 
         end
 
-        pokemonInfo = {
+        pokemon_info = {
             name: res.name,
             id: res.id,
-            types: pokemonType,
-
+            types: pokemon_type,
         }
-        return pokemonInfo.each do |key, value|
-            puts key.to_s + " => " + value.to_s
+        return pokemon_info.each do |key, value|
+            puts key.to_s.capitalize + " => " + value.to_s.capitalize
         end
     end
 
-    def getPokemonSpeciesInfo(pokemonName) 
-        res = PokeApi.get(pokemon_species: pokemonName)
-        desc = res.flavor_text_entries[0].flavor_text.gsub(/\n/, " ").gsub(/\u000c/, " ")
+    def get_pokemon_species_info(pokemon_name) 
+        res = PokeApi.get(pokemon_species: pokemon_name)
+        evo_id = res.evolution_chain.get.id
+        evo_res = PokeApi.get(evolution_chain: evo_id)
+        evo_chain = evo_res.chain.evolves_to.first.species.name
+        desc = res.flavor_text_entries[1].flavor_text.gsub(/\n/, " ").gsub(/\u000c/, " ")
 
-        pokemonInfo = {
+        pokemon_info = {
             generation: res.generation.name,
+            habitat: res.habitat.name,
+            shape: res.shape.name,
+            colour: res.color.name,
             description: desc,
         }
-        return pokemonInfo.each do |key, value|
-            puts key.to_s + " => " + value.to_s
+
+        begin
+            puts "Evolves Into => " + evo_chain.to_s.capitalize
+        rescue 
         end
 
+        return pokemon_info.each do |key, value|
+            puts key.to_s.capitalize + " => " + value.to_s.capitalize
+        end
     end
 
+
+    # def get_pokemon_evo_chain(pokemon_name)
+    #     res = PokeApi.get(evolution_chain: )
+
+    # def get_pokemon_evo_chain(pokemon_name)
+    #     res = PokeApi.get(pokemon_species: pokemon_name)
+
+    #     pokemon_info = {
+            
+    #     }
+    #     # evo_chain = 
+    # end
+
+    def get_all_pokemon_names
+        res = PokeApi.get(pokemon: {limit:2000, offset: 0})
+        pokemon_info = {
+            all_name: res.name,
+        }
+        return pokemon_info.each do |key, value|
+            puts key.to_s.capitalize + " => " + value.to_s.capitalize
+        end
+    end
 
 end
 
--- src/main.rb
+++ src/main.rb
@@ -23,40 +23,51 @@ self.logo
 prompt = TTY::Prompt.new
 
 #Obtain and save user_name
-user_name = prompt.ask("Hello Pokemon Trainer! What is your name?")
-user_name = user_name.capitalize 
+begin
+    puts ("\n") * 3
+    user_name = prompt.ask("Hello Pokemon Trainer! What is your name?")
+    raise StandardError if user_name.nil? || user_name == ""
+rescue
+    puts "Please enter a name."
+    retry
+else
+user_name = user_name.capitalize
+end
 
 #Saves menu choices in a variable as a hash of key value pairs
 #Select from prompted choices, allows cycling of menu once selection reaches bottom of options and oves user to specific interface as per selection
 #
 while true
     choices = {"Use Pokedex": 1, "Upload Your Team": 2, "Exit": 3}
+    puts ("\n") * 2
     choice = prompt.select("Welcome to the Pokedex App Trainer #{user_name}! What would you like to do?", choices, cycle: true)
     case choice
     when 1
+        puts ("\n") * 2
         choices = {"Search Specific Pokemon": 1, "Browse Pokemon Database": 2, "Go Back": 3}
         choice = prompt.select("Accessing Pokedex database ... What would you like to do?", choices, cycle: true)
+        puts ("\n") * 2
         case choice
             when 1 
                 choice = prompt.ask("Which Pokemon would you like to search?")
                 choice = choice.downcase
                 search_result = Pokedex.new(choice)
-                search_result.getPokemonGeneralInfo(choice)
-                search_result.getPokemonSpeciesInfo(choice)
-                # search_again = prompt.yes?("Would you like to perform another search?")
-                #     search_again == 1
-                #     if search_again == 1
-                #         prompt.ask("Which Pokemon would you like to search?")
-                    # end
+                search_result.get_pokemon_general_info(choice)
+                search_result.get_pokemon_species_info(choice)
             when 2
-                
+                puts ("\n") * 2
+                choice = choice.get_all_pokemon_names(choice)
+                puts ("\n") * 2
             when 3
         end
     when 2
+        puts ("\n") * 2
         puts "in progress"
         #parse json file of pokemon team to pull data for whole team
     when 3
+        puts ("\n") * 2
         puts "Goodbye Trainer #{user_name}!"
+        puts ("\n") * 2
         exit
     end
 end
